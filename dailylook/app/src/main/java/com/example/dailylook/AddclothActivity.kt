21f3eb1f621074a8d2d9a372f47c3ad979bde012
hyunho <melbua@naver.com>
2022-06-10 20:39:12 +0900

    의류 추가화면 병합

---
+++ dailylook/app/src/main/java/com/example/dailylook/AddclothActivity.kt
@@ -0,0 +1,11 @@
+package com.example.dailylook
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddclothActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_addcloth)
+    }
+}
